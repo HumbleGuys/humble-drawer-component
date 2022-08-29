1fcb1287c6449e8c63a5fbd920fd46394f8803cb
Kim Larsson <kim@humbleguys.se>
2022-08-29 09:35:49 +0200

    Test pushing css / js

--- src/resources/components/base.blade.php
+++ src/resources/components/base.blade.php
@@ -1,3 +1,10 @@
-<div>
+<div class="drawer">
     {!! $slot !!}
 </div>
+
+@once
+    @push('head')
+        <link rel="stylesheet" href="{{ asset('../vendor/humble-guys/humble-drawer-component/src/resources/css/drawer.css') }}">
+        <script src="{{ asset('../vendor/humble-guys/humble-drawer-component/src/resources/js/drawer.js') }}"></script>
+    @endpush   
+@endonce 
