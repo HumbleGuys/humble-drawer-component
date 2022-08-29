616f571fba5fac4c58ae9946ebb03b5cff49c5ba
Kim Larsson <kim@humbleguys.se>
2022-08-29 10:03:08 +0200

    Alpine test

--- src/resources/components/base.blade.php
+++ src/resources/components/base.blade.php
@@ -1,10 +1,28 @@
-<div class="drawer">
+@props([
+    'name',
+    'transitionName' => null,
+    'activeBodyClass' => '',
+    'defaultOpen' => false
+])
+
+<div
+    x-data="drawer({
+        id: '{{ $name }}',
+        activeBodyClass: '{{ $activeBodyClass }}',
+        defaultOpen: {{ json_encode($defaultOpen) }}
+    })"
+    x-show="isOpen"
+    class="drawer"
+    role="region"
+    tabindex="-1"
+    x-cloak
+>
     {!! $slot !!}
 </div>
 
 @once
     @push('head')
         <link rel="stylesheet" href="{{ asset('../vendor/humble-guys/humble-drawer-component/src/resources/css/drawer.css') }}">
-        <script src="{{ asset('../vendor/humble-guys/humble-drawer-component/src/resources/js/drawer.js') }}"></script>
+        <script module defer src="{{ asset('../vendor/humble-guys/humble-drawer-component/src/resources/js/drawer.js') }}"></script>
     @endpush   
 @endonce 
--- src/resources/js/drawer.js
+++ src/resources/js/drawer.js
@@ -1,1 +1,67 @@
-console.log('drawer');
+const drawer = ({ id, activeBodyClass, defaultOpen = false }) => ({
+    isOpen: false,
+
+    init () {
+        if (defaultOpen) {
+            this.open();
+        }
+
+        Alpine.effect(() => {
+            if (this.$store.drawer.id === id) {
+                this.isOpen = true;
+                document.body.style.overflow = 'hidden';
+
+                if (activeBodyClass) {
+                    document.body.classList.add(activeBodyClass);
+                }
+            } else {
+                this.isOpen = false;
+                document.body.style.overflow = '';
+
+                if (activeBodyClass) {
+                    document.body.classList.remove(activeBodyClass);
+                }
+            }
+        });
+
+        document.addEventListener('keydown', (event) => {
+            if (!this.isOpen) {
+                return;
+            }
+
+            if (event.key === 'Escape') {
+                this.close();
+            }
+        });
+    },
+
+    toggle (id) {
+        this.$store.drawer.toggle(id);
+    },
+
+    close () {
+        this.$store.drawer.close();
+    },
+
+    open () {
+        this.$store.drawer.open(id);
+    }
+});
+
+window.Alpine.data('drawer', drawer);
+
+window.Alpine.store('drawer', {
+    id: null,
+
+    toggle (id) {
+        this.id = this.id === id ? null : id;
+    },
+
+    open (id) {
+        this.id = id;
+    },
+
+    close () {
+        this.id = null;
+    }
+});
